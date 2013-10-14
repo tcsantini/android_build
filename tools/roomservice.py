f651de419e2d23204ae8c3e146c9dec56a4e8ba4
DerTeufel <dominik-kassel@gmx.de>
2013-10-14 06:49:24 +0200

    roomservice: improved handling of remote repos (thx to cyankang)
    Change-Id: Ic5fce9452780821c094949ea08fa824a4742274f
    
    debug
    
    Change-Id: I0ff3b22fb10d490acf9d450050ed12b2ae1ea012

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -51,7 +51,7 @@ except:
     device = product
 
 if not depsonly:
-    print("Device %s not found. Attempting to retrieve device repository from Hellybean Github (http://github.com/Hellybean)." % device)
+    print("Device %s not found. Attempting to retrieve device repository from Hellybean Github or Cyanogenmod Github." % device)
 
 repositories = []
 
@@ -71,7 +71,7 @@ def add_auth(githubreq):
 
 page = 1
 while not depsonly:
-    githubreq = urllib.request.Request("https://api.github.com/users/Hellybean/repos?per_page=200&page=%d" % page)
+    githubreq = urllib.request.Request("https://api.github.com/users/CyanogenMod/repos?per_page=200&page=%d" % page)
     add_auth(githubreq)
     result = json.loads(urllib.request.urlopen(githubreq).read().decode())
     if len(result) == 0:
@@ -169,13 +169,25 @@ def add_to_manifest(repositories, fallback_branch = None):
     for repository in repositories:
         repo_name = repository['repository']
         repo_target = repository['target_path']
+        rom = None
+        
+        # Check if repo exists in Hellybean repo tree
+        req = urllib.request.Request("https://github.com/Hellybean/%s" % repo_name)
+        try:
+            urllib.request.urlopen(req)
+            rom = 'Hellybean'
+        except urllib.error.HTTPError as e:
+            rom = 'CyanogenMod'
+
+	print("Found repo at: https://github.com/%s/%s" % (rom, repo_name))
+
         if exists_in_tree(lm, repo_name):
-            print('%s already exists' % (repo_name))
+            print('%s/%s already exists' % (rom, repo_name))
             continue
 
-        print('Adding dependency: Hellybean/%s -> %s' % (repo_name, repo_target))
+        print('Adding dependency: %s/%s -> %s' % (rom, repo_name, repo_target))
         project = ElementTree.Element("project", attrib = { "path": repo_target,
-            "remote": "github", "name": "Hellybean/%s" % repo_name })
+            "remote": "github", "name": "%s/%s" % (rom, repo_name) })
 
         if 'branch' in repository:
             project.set('revision',repository['branch'])
@@ -206,7 +218,7 @@ def fetch_dependencies(repo_path, fallback_branch = None):
         fetch_list = []
 
         for dependency in dependencies:
-            if not is_in_manifest("Hellybean/%s" % dependency['repository']):
+            if not is_in_manifest("CyanogenMod/%s" % dependency['repository']) and not is_in_manifest("Hellybean/%s" % dependency['repository']):
                 fetch_list.append(dependency)
                 syncable_repos.append(dependency['target_path'])
 
